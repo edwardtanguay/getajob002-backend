eca94b74952fa75879adb4a51d4e6d6d6fc0ef96
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-06 11:56:17 +0100

    finished day 1

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,17 @@
-import { createServer } from 'http';
-import { mainContent } from './content.js';
+import express from 'express';
+import fs from 'fs';
+import cors from 'cors';
+const app = express();
+app.use(cors());
 const port = 3011;
-createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text-plain' });
-    res.write(mainContent);
-    res.end();
-}).listen(port);
-console.log(`listening on http://localhost:${port}`);
+const jobs = JSON.parse(fs.readFileSync('./src/data/jobs.json', 'utf8'));
+app.get('/', (req, res) => {
+    res.send('job site api');
+});
+app.get('/jobs', (req, res) => {
+    res.json(jobs);
+});
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
+});
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
-import { createServer } from 'http';
-import { mainContent } from './content.js';
+import express from 'express';
+import fs from 'fs';
+import cors from 'cors';
 
+const app = express();
+app.use(cors());
 const port = 3011;
 
-createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text-plain' });
-	res.write(mainContent);
-	res.end();
-}).listen(port);
+const jobs = JSON.parse(fs.readFileSync('./src/data/jobs.json', 'utf8'));
 
-console.log(`listening on http://localhost:${port}`);
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.send('job site api');
+});
+
+app.get('/jobs', (req: express.Request, res: express.Response) => {
+	res.json(jobs);
+})
+
+app.listen(port, () => {
+	console.log(`listening on http://localhost:${port}`);
+});
